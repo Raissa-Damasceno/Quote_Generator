1aacffe26ebf2b154f514ccfcfce20b9758cbac3
Raissa <raissa.cod@gmail.com>
2024-04-08 17:11:34 +1000

    loader added

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+const complete = () => {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+};
 
 const newQuote = () => {
+  loading();
   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
 
   quote.author
@@ -16,6 +28,15 @@ const newQuote = () => {
     : quoteText.classList.remove("long-quote");
 
   quoteText.textContent = quote.text;
+  complete();
 };
+
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+};
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 newQuote();
